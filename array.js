973c5f44d47ff286be590d8086eae298b4cff2fd
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-07-31 00:39:56 +0600

    array

---
+++ array.js
@@ -0,0 +1,21 @@
+let num1 = 2;
+let num2 = 10;
+let num3 = 67;
+let num4 = 98;
+// ekat value thakte parent, onekgula value ow thakte pare plus faka ow thakte pare
+// let numbers=[]
+// let numbers =[4] 
+let numbers =[1, 2, 3, 10, 67, 18, 98]; //Collection of multiple items under a single variable name
+
+// first bracket ()
+// second braket {}
+// third bracket []
+
+for(let num of numbers){
+    console.log(num);
+}
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let friends = ['abul', 'kabul', 'bulbul', 'bulbul'];
+let passed = [true, false, true, false];
+let mixedArray = [12, 'Abul', true, 5.667];
